f4c31a284b933d8cd7577e4879ff8385b1403fda
CesarRoldan99 <77574791+CesarRoldan99@users.noreply.github.com>
2021-03-26 18:48:32 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -6,23 +6,23 @@ import pickle
 print(os.getcwd())
 path = os.getcwd()
 
-with open('Models/LogisticRegressionModel.pkl', 'rb') as f:
+with open('Models/LogisticRegressionMod.pkl', 'rb') as f:
     logistic = pickle.load(f)
 
-with open('Models/RandomForestModel.pkl', 'rb') as f:
-    randomforest = pickle.load(f)
-
-with open('Models/SVMModel.pkl', 'rb') as f:
+with open('Models/SVMMod.pkl', 'rb') as f:
     svm_model = pickle.load(f)
 
+with open('Models/RandomForestMod.pkl', 'rb') as f:
+    randomforest = pickle.load(f)
 
-def get_predictions(Age, Sex, Chest_Pain, Resting_Blood_Pressure, Serum_Cholestoral,
-                    Fasting_Blood_Sugar, Resting_ECG_Results, Maximum_HeartRate,
-                    Exercise_Induced_Angina, STDepression_Induced, req_model):
 
-    mylist = [Age, Sex, Chest_Pain, Resting_Blood_Pressure, Serum_Cholestoral,
-              Fasting_Blood_Sugar, Resting_ECG_Results, Maximum_HeartRate,
-              Exercise_Induced_Angina, STDepression_Induced, req_model]
+def get_predictions(age, sex, chest_pain, resting_blood_pressure, serum_cholestoral, fasting_blood_sugar,
+                    resting_ecg_results, maximum_heartrate, exercise_induced_angina, stdepression_induced,
+                    slope, mayor_vessels,thal, req_model):
+
+    mylist = [age, sex, chest_pain, resting_blood_pressure, serum_cholestoral, fasting_blood_sugar,
+                    resting_ecg_results, maximum_heartrate, exercise_induced_angina, stdepression_induced,
+                    slope, mayor_vessels, thal]
 
     mylist = [float(i) for i in mylist]
     vals = [mylist]
@@ -53,28 +53,33 @@ def homepage():
 @app.route('/', methods=['POST', 'GET'])
 def my_form_post():
     if request.method == 'POST':
-        Age = request.form['Age']
-        Sex = request.form['Sex']
-        Chest_Pain = request.form["Chest_Pain"]
-        Resting_Blood_Pressure = request.form["Resting_Blood_Pressure"]
-        Serum_Cholestoral = request.form["Serum_Cholestoral"]
-        Fasting_Blood_Sugar = request.form["Fasting_Blood_Sugar"]
-        Resting_ECG_Results = request.form["Resting_ECG_Results"]
-        Maximum_HeartRate = request.form["Maximum_HeartRate"]
-        Exercise_Induced_Angina = request.form["Exercise_Induced_Angina"]
-        STDepression_Induced = request.form["STDepression_Induced"]
+
+
+        age = request.form['Age']
+        sex = request.form['Sex']
+        chest_pain = request.form['Chest_Pain']
+        resting_blood_pressure = request.form['Resting_Blood_Pressure']
+        serum_cholestoral = request.form['Serum_Cholestoral']
+        fasting_blood_sugar = request.form['Fasting_Blood_Sugar']
+        resting_ecg_results = request.form['Resting_ECG_Results']
+        maximum_heartrate = request.form['Maximum_HeartRate']
+        exercise_induced_angina = request.form['Exercise_Induced_Angina']
+        stdepression_induced = request.form['STDepression_Induced']
+        slope = request.form['Slope']
+        mayor_vessels = request.form['Mayor_Vessels']
+        thal = request.form['Thal']
         req_model = request.form['req_model']
 
-        target = get_predictions(Age, Sex, Chest_Pain, Resting_Blood_Pressure, Serum_Cholestoral,
-                                 Fasting_Blood_Sugar, Resting_ECG_Results, Maximum_HeartRate,
-                                 Exercise_Induced_Angina, STDepression_Induced, req_model)
+        target = get_predictions(age, sex, chest_pain, resting_blood_pressure, serum_cholestoral, fasting_blood_sugar,
+                    resting_ecg_results, maximum_heartrate, exercise_induced_angina, stdepression_induced,
+                    slope, mayor_vessels,thal, req_model)
 
         if target==1:
-            sickness_making = 'Customer is likely to have Cardiac Disease'
+            status = 'Patient probably has a heart condition'
         else:
-            sickness_making = 'Customer is unlikely to have Cardiac Disease'
+            status = 'Patient is completely fine'
 
-        return render_template('home.html', target = target, sale_making = sickness_making)
+        return render_template('home.html', target = target, status = status)
     else:
         return render_template('home.html')
 
